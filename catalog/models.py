c161a7a020147cc429c3030382ce9ce30730b2ed
Paul <comixan@gmail.com>
2015-05-29 13:04:12 +0300

    new features

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(Category)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "text", "author"]
+    search_fields = ["title", "text", "author"]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from models import Post
 # Create your views here.
 def index(request):
-    return render(request, 'catalog/index.html', {})
+    category = request.GET.get('c', 'levis')
+    posts = Post.objects.filter( author__name = category ).order_by( '-id' )
+
+    return render(request, 'catalog/index.html', {'posts': posts})
